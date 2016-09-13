9890719d06656833464702fc4c6b3dd22df23c40
ChandrashekarChary <ChandrashekarChary@chandhu_P>
2016-09-12 23:51:07 -0500

    Initial Commit

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -1,3 +1,7 @@
+"""
+Created on August 30,2016
+@author: Chandrashekar Chary Vadla
+"""
 from test.test_socket import try_address
 from math import degrees
 from macpath import split
@@ -13,37 +17,40 @@ class Angle():
     def setDegrees(self,degrees=None):
         """degrees is the number of degrees (and portions of degrees).It is a numeric value (integer or float). 
         Optional, defaults to zero if missing."""
-        degreeString=str(degrees)  
+        degreeString=str(degrees) #degreeString is input string degrees>whic is used for splitting used in line 23
         if(degrees is int or float):        
-            if('.' not in degreeString):#
+            if('.' not in degreeString):#if its not a float value
                 degrees=degrees%360
                 self.degrees=degrees
                 self.minutes=0                 
             else:
-                degreeSplit=degreeString.split(".")
+                degreeSplit=degreeString.split(".")#if length of degreeSplit[1])>1 then its having more than one decimal gives error
                 if len(degreeSplit[1])>1:
                     raise ValueError("error: minutes having more than one decimal")
                 else:
                     degrees= degrees%360
                     degreeString1=str(degrees)
                     degreeSplit1=degreeString1.split(".")
-                    
+                    #degreeSplit1 is string of degrees
+                    #make sense assigning values to degreees and minutes 
+
                     self.degrees=int(degreeSplit1[0])
                     self.minutes=(int(degreeSplit1[1])*60)/10
         else:
             raise ValueError("error: degrees is not a integer/float")       
         return degrees   
     def setDegreesAndMinutes(self, angleString=None):
-        if ('d' in angleString):
+        if ('d' in angleString):#valid string having 'd' in it,so im checking d first
             splitAnglestring=angleString.split("d")#used to split anglestring
-            if(int(splitAnglestring[0])):
+            if(int(splitAnglestring[0])):#splitAnglestring[0] should be integer value otherwise returns error
                 splitAnglestring[0]=int(splitAnglestring[0])
                 degrees=self.setDegrees(splitAnglestring[0])
             else:
                 raise ValueError("invalid angleString")
+            #checks the splitAnglestring[1]  is int or float 
             if('.' in splitAnglestring[1] or float(splitAnglestring[1]) or int(splitAnglestring[1])):
                 splitAnglestringMin=splitAnglestring[1].split(".")
-                if(len(splitAnglestringMin)>2):
+                if(len(splitAnglestringMin)>2):#if decimal more than 2 gives error?
                     raise ValueError("Invalid angleString please give valid input")
                 else:
                     if(float(splitAnglestring[1])>60):
@@ -63,12 +70,16 @@ class Angle():
     
         else:
             raise ValueError("Invalid angleString please give valid input")
-    
+    """
+    takes the input angle into degrees string
+    checks the if the  int(degrees[0]) and int(degrees[1])) or float(degrees[1]) or gives error
+    for add subtraction and compare
+    """
     def add(self, angle=None):
         
         degrees=angle.split("d")
         try:
-            if((int(degrees[0]) and int(degrees[1])) or float(degrees[1])):
+            if(int(degrees[0])):
                
                 self.degrees +=int(degrees[0])
                 self.minutes +=float(degrees[1])
@@ -77,13 +88,13 @@ class Angle():
                 Minute=round(float(self.minutes/60),1)
                 Degrees= Minute+self.degrees
                 Degrees=Degrees%360
-                return Degrees
+                return Degrees#returns sum of degrees
         except:
             raise ValueError("invalid adding angle in instance add")
     def subtract(self, angle):
         degrees=angle.split("d")
         try:
-            if((int(degrees[0]) and int(degrees[1])) or float(degrees[1])):
+            if(int(degrees[0])):
                 self.degrees -=int(degrees[0])
                 self.minutes -=float(degrees[1])
         
@@ -91,14 +102,21 @@ class Angle():
                 Minute=round(float(self.minutes/60),1)
                 Degrees= Minute+self.degrees
                 Degrees=Degrees%360
-                return Degrees
+                return Degrees#returns subtraction of degrees
         except:
             raise ValueError("invalid adding angle in instance subtract")
+        """
+        compDegree is takes angle1
+        degrees takes input angle by method
+        compares the angles,return 1(if its greater compDegree )
+        return 0 (if its equal angles)
+        return -1 ( (if its lesser compDegree )
+        """
     def compare(self, angle): 
         compDegree=self.getDegrees()
         degrees=angle.split("d")
         try:
-            if((int(degrees[0]) and int(degrees[1])) or float(degrees[1])):
+            if(int(degrees[0])):
                 self.degrees =int(degrees[0])
                 self.minutes =float(degrees[1])
                 Minute=self.minutes
@@ -119,7 +137,7 @@ class Angle():
         degrees=str(self.degrees)
         minutes=str(self.minutes)
         string=degrees+"d"+minutes
-        return string
+        return string#gives the output of degrees in string mode means: Example:240d35
     
     def getDegrees(self):
         Minute=round(float(self.minutes)/60,1)
--- SoftwareProcess/Navigation/test/__init__.py
+++ SoftwareProcess/Navigation/test/__init__.py
@@ -10,16 +10,27 @@ angle2Degrees = angle2.setDegrees(degrees=50.5)
 #angle3Degrees = angle3.setDegreesAndMinutes("36d5")   #angle3Degrees should be 0.5 
 #angle4Degrees = angle4.setDegreesAndMinutes("15d5")     
 #addangle1=angle1.add("43d4")
-#addangle2=angle2.add("15d0") 
-#subangle1=angle2.subtract("-386d30")
-#anglestring1=angle2.getString()
- 
-#print angle3Degrees
-print angle2Degrees
+addangle2=angle2.add("15d0") 
+subangle1=angle2.subtract("86d30")
 compAngle1=angle2.compare("25d3")
+#anglestring1=angle2.getString()
+print("input angle:")
+print angle2Degrees
+#print angle3Degrees
+print('addition of angles:')
+print addangle2
+print("subtraction of angles")
+print subangle1
+print("comparision of angles")
 print compAngle1
+anglestring1=angle2.getString()
+print("convertedvstring mode angle")
+print anglestring1
+#print angle2Degrees
 
-#print addangle2
+#print compAngle1
+
+
 #print subangle1
 
 #angle1Degrees=angle1Degrees+angle2Degrees
